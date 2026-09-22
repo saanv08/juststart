6447557c6a37b0a0bb165330f69e3c74489ed537
saanv08 <saanvissuman@gmail.com>
2026-09-22 16:46:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+var C="just-start-v2",A=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png"];
+self.addEventListener("install",function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(A)}).then(function(){return self.skipWaiting()}))});
+self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!=C}).map(function(x){return caches.delete(x)}))}).then(function(){return self.clients.claim()}))});
+self.addEventListener("fetch",function(e){if(e.request.method!="GET"||new URL(e.request.url).origin!=location.origin)return;e.respondWith(fetch(e.request).then(function(r){var c=r.clone();caches.open(C).then(function(x){x.put(e.request,c)});return r}).catch(function(){return caches.match(e.request).then(function(m){return m||caches.match("./index.html")})}))});
